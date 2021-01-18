ce03f373b399e483777c0794e3c8aa9773858b86
dmalzl <UaF20091!>
2021-01-18 15:48:27 +0100

    first commit of full script

--- bin/splitSamByChromosome.py
+++ bin/splitSamByChromosome.py
@@ -8,9 +8,9 @@ def get_header_from_samfile(file)
     return f.header
 
 
-def initialize_outputfiles(nameprefix, chromosomes, header):
+def initialize_outputfiles(nameprefix, header):
     handle_dict = {}
-    for chrom in chromosomes:
+    for chrom in header.references:
         filename = nameprefix + '_' + chrom + '.sam'
         handle_dict[chrom] = ps.AlignmentFile(filename, 'w', header = header)
 
@@ -27,9 +27,27 @@ args = parser.parse_args()
 
 samheader = get_header_from_samfile(args.samfiles[0])
 
-outfile_dict = initialize_outputfiles(args.outputPrefix,
-                                      samheader.references,
-                                      header)
+outfile_handles = initialize_outputfiles(args.outputPrefix,
+                                         samheader.references,
+                                         header)
 
 for file in args.samfiles:
-    samfile = ps.AlignmentFile(file, 'r')
+    with ps.AlignmentFile(file, 'r') as samfile:
+        while True:
+            try:
+                r1 = samfile.__next__()
+                r2 = samfile.__next__()
+
+            except StopIteration:
+                break
+
+            chrom1 = r1.reference_name
+            chrom2 = r2.reference_name
+            handle_key = chrom1 if chrom1 < chrom2 else chrom2
+
+            for r in [r1, r2]:
+                outfile_handles[handle_key].write(r)
+
+
+for key, filehandle in outfile_handles.items():
+    filehandle.close()
